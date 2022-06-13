43e9ba8d6985de09f7996afaa402ff5aa477ecb8
Tresor-ilunga <19im065@esisalama.org>
2022-06-13 09:58:53 +0200

    Ajout du mode sombre

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById('header')
-    // // Lorsque le défilement est supérieur à la hauteur de 50 fenêtres, ajoutez la classe scroll-header à la balise d'en-tête.
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+        // // Lorsque le défilement est supérieur à la hauteur de 50 fenêtres, ajoutez la classe scroll-header à la balise d'en-tête.
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -11,22 +12,22 @@ const modalViews = document.querySelectorAll('.services__modal'),
     modalBtns = document.querySelectorAll('.services__button'),
     modalClose = document.querySelectorAll('.services__modal-close')
 
-let modal = function(modalClick){
+let modal = function(modalClick) {
     modalViews[modalClick].classList.add('active-modal')
 }
 
-modalBtns.forEach((mb, i) =>{
-	mb.addEventListener('click', () =>{
-		modal(i)
-	})
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
 })
 
-modalClose.forEach((mc) =>{
-	mc.addEventListener('click', () =>{
-		modalViews.forEach((mv) =>{
-			mv.classList.remove('active-modal')
-		})
-	})
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
 })
 
 
@@ -40,14 +41,14 @@ let mixerPortfolio = mixitup('.work__container', {
     }
 });
 
-/* Link active work */ 
+/* Link active work */
 const linkWork = document.querySelectorAll('.work__item')
 
-function activeWork(){
-    linkWork.forEach(l=> l.classList.remove('active-work'))
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'))
     this.classList.add('active-work')
 }
-linkWork.forEach(l=> l.addEventListener('click', activeWork))
+linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 let swiperTestimonial = new Swiper('.testimonial__container', {
@@ -73,23 +74,51 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
+    /*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
 
+// thème précédemment sélectionné (si l'utilisateur l'a sélectionné)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// Nous obtenons le thème actuel de l'interface en validant la classe light-theme
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// Nous validons si l'utilisateur a précédemment choisi un thème
+if (selectedTheme) {
+    // Si la validation est effectuée, nous demandons quel était le problème pour savoir si nous avons activé ou désactivé la lumière.
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activez / désactivez le thème manuellement avec le bouton
+themeButton.addEventListener('click', () => {
+    // Ajouter ou supprimer le thème de la lumière / des icônes
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+        // Nous sauvegardons le thème et l'icône actuelle que l'utilisateur a choisis.
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
@@ -100,5 +129,5 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data`)
-sr.reveal(`.home__img`, {delay: 700})
-sr.reveal(`.home__social, .home__scroll`,{delay: 900, origin: 'bottom'})
+sr.reveal(`.home__img`, { delay: 700 })
+sr.reveal(`.home__social, .home__scroll`, { delay: 900, origin: 'bottom' })
